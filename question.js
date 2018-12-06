30f5d6cadce38e0b14e75a4f34813abdbb4ea151
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2018-12-06 16:58:45 +0000

    Quiz playable with timer and score at the end

---
+++ question.js
@@ -0,0 +1,74 @@
+var questions = [{
+  "question": "What movie has one the most Golden Globe Awards with seven?",
+  "option1": "La La Land",
+  "option2": "A Star is Born",
+  "option3": "The Godfather",
+  "option4": "Doctor Zhivago",
+  "answer": "1"
+}, {
+  "question": "Which celebrity was not born in the U.S?",
+  "option1": "Arnold Schwarzenegger",
+  "option2": "Chris Pratt",
+  "option3": "Micheal Jordan",
+  "option4": "Kanye West",
+  "answer": "1"
+}, {
+  "question": "Which of these shows has been hosted by Jimmy Fallon since 2014?",
+  "option1": "The Late Show",
+  "option2": "Late Night",
+  "option3": "The Daily Show",
+  "option4": "The Tonight Show",
+  "answer": "4"
+}, {
+  "question": "Which of these musicals was not composed by Andrew Lloyd Webber?",
+  "option1": "Jesus Christ Supertar",
+  "option2": "Chicago",
+  "option3": "Cats",
+  "option4": "The Phantom of the Opera",
+  "answer": "2"
+}, {
+  "question": "What Broadway show includes characters such as Riff, Tony, and Maria?",
+  "option1": "Rent",
+  "option2": "West Side Story",
+  "option3": "Hairspray",
+  "option4": "Grease",
+  "answer": "2"
+}, {
+  "question": "Which of these entertainment mediums is a TV show?",
+  "option1": "30 Rock",
+  "option2": "Evita",
+  "option3": "Office Space",
+  "option4": "A Day in the Life",
+  "answer": "1"
+}, {
+  "question": "Which of these quotes is not from a Star Wars movie?",
+  "option1": "No, I am your father.",
+  "option2": "May the force be with you.",
+  "option3": "That's no moon.",
+  "option4": "Here's Johnny!",
+  "answer": "4"
+}, {
+  "question": "Which of these comic book superheroes does not belong to Marvel?",
+  "option1": "Wonder Woman",
+  "option2": "The Hulk",
+  "option3": "Deadpool",
+  "option4": "Ant-Man",
+  "answer": "1"
+}, {
+  "question": "What anime features characters that can become 'Super Saiyan'?",
+  "option1": "Naruto",
+  "option2": "Dragon Ball Z",
+  "option3": "One Piece",
+  "option4": "One Punch Man",
+  "answer": "2"
+}, {
+  "question": "Which of these superhero ensembles belongs to DC Comics?",
+  "option1": "The Avengers",
+  "option2": "The Justice League",
+  "option3": "Teenage Mutant Ninja Turtles",
+  "option4": "X-Men",
+  "answer": "2"
+
+}
+
+];
