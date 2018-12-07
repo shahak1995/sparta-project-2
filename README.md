25607be398deb6d96cb3f3e69f1386d4e4bb794f
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2018-12-07 11:23:18 +0000

    Updated the timer to 1 min, added the home page and did the read me

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # sparta-project-2
+
+### Description
+My game is a Entertainment quiz game in which the player has to answer 10 questions with 4 choices in a span of 1 minute and then at the end of the quiz it will display how much the user has scored out of 100. 
+
+### Languages used
+* HTML
+* CSS
+* Javascript
+
+### How to download
+1. Type in on github sparta-project-2
+2. Click on the repo that says shahak1995/sparta-project-2
+3. You then click clone or download and download the zip file
+
+(If its hosted on Github Pages then...)
+#### You can view the page [here]()
+
+### Challenges (you can include images and code block here)
+1. Putting the information of the questions and answer in code and make it display in their containers
+2. Setting the timer to stop when it reaches 0 and display an alert saying that the time has ran out
+3. Laying out the question choices and selecting which one is the answer through css using arrays
+
+### Learning points
+1. Aligning elements with use of css
+2. Using javascript to control html elements with functions
+3. Displaying each elements id through use of javascript
--- timer.js
+++ timer.js
@@ -1,22 +1,46 @@
-document.getElementById("timer").innerHTML =
-  03 + ":" + 00;
-startTimer();
+var timeLeft = 59;
+var elem = document.getElementById('timer');
 
-function startTimer() {
-  var presentTime = document.getElementById('timer').innerHTML;
-  var timeArray = presentTime.split(/[:]+/);
-  var m = timeArray[0];
-  var s = checkSecond((timeArray[1] - 1));
-  if (s == 59) { m = m - 1 }
-  if (m < 0) { alert('Times Up!') }
-  document.getElementById('timer').innerHTML =
-    m + ":" + s;
-  setTimeout(startTimer, 1000);
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+  if (timeLeft == 00 + 00) {
+    clearTimeout(timerId);
+    setTimeout(function () { alert("TIMES UP!"); }, 1000);
+
+  } else {
+    elem.innerHTML = timeLeft;
+    timeLeft--;
+  }
 }
 
-function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) { sec = "0" + sec };
-  if (sec < 0) { sec = "59" };
-  return sec;
-}
+
+
+
+
+
+// document.getElementById("timer").innerHTML =
+//   00 + ":" + 10;
+// startTimer();
+
+// function startTimer() {
+//   var presentTime = document.getElementById('timer').innerHTML;
+//   var timeArray = presentTime.split(/[:]+/);
+//   var m = timeArray[0];
+//   var s = checkSecond((timeArray[1] - 1));
+//   if (s == 59) { m = m - 1 }
+//   document.getElementById('timer').innerHTML =
+//     m + ":" + s;
+//   setTimeout(startTimer, 1000);
+//   if (m < 0) {
+//     setTimeout(function () { alert("Times Up!"); }, 1000);
+//   }
+
+// }
+
+// function checkSecond(sec) {
+//   if (sec < 10 && sec >= 0) { sec = "0" + sec };
+//   if (sec < 0) { sec = "59" };
+//   return sec;
+// }
 
